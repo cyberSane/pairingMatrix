792fc36082ce6f9edd7a826d070d9288947a1bc2
abhikur <thakura204@gmail.com>
2017-05-27 16:51:44 +0530

    |Abhishek| Adding a commit data provider which provide the massaged data out of commits

---
+++ tests/lib/commitDataProvider.test.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var DataProvider = require('../../lib/commitDataProvider');
+
+describe('github data provider', function() {
+
+	var dataFetcher;
+
+	before(function() {
+		dataFetcher = {
+			fetch: function() {
+				return ['c3x4211 |Abhishek| Adding package.json',
+						'd3x8111 |Abhishek| Adding a gitignore file',
+						'f3x2121 |Abhishek/Abhikur| Initial commit '
+						]
+			}
+		}
+	});
+
+	it('should get commits data', function() {
+
+		var regex = /\|\w*\/\w*\||\|\w*\|/gi;
+		var dataProvider = new DataProvider(dataFetcher, regex);
+
+		var data = dataProvider.provideData();
+
+		assert.equal(data.indivisuals.length, 1);
+		assert.equal(data.indivisuals[0].pair[0], 'Abhishek')
+		assert.equal(data.indivisuals[0].commits, 2)
+
+		assert.equal(data.validPairs.length, 1)
+		assert.equal(data.validPairs[0].pair[0], 'Abhishek')
+		assert.equal(data.validPairs[0].pair[1], 'Abhikur')
+		assert.equal(data.validPairs[0].commits, 1)
+
+		assert.equal(data.committers.length, 2);
+
+	});
+})
